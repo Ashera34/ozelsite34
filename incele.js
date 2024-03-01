96d0aa3b151f51659fef106ddaea3d625955cc2f
Ashera34 <kaptanopx@gmail.com>
2024-03-01 20:25:08 +0300

    a12

--- incele.js
+++ incele.js
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Page Title</title>
-<script>
+
     // Sağ tıklama olayını engelleme
     document.addEventListener('contextmenu', function (e) {
         e.preventDefault();
@@ -28,11 +22,3 @@
             return false;
         }
     }
-</script>
-</head>
-<body>
-<!-- Sayfanızın içeriği buraya gelecek -->
-<h1>Welcome to My Page</h1>
-<p>This is my webpage.</p>
-</body>
-</html>
